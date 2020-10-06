f3a2278cba405703831d5760583aab02a095d7dc
Artem Bakhanov <artembakhanov@gmail.com>
2020-10-06 16:10:28 +0300

    Fix bugs and save to file

--- src/main/scala/StreamProcesser.scala
+++ src/main/scala/StreamProcesser.scala
@@ -1,30 +1,41 @@
+import org.apache.spark.ml.classification.LogisticRegressionModel
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
-import org.apache.spark.sql.{Row, SQLContext, SparkSession}
+import org.apache.spark.sql.{Row, SQLContext, SaveMode, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext, Time}
 import org.apache.spark.{SparkConf, SparkContext}
 
 
 object StreamProcesser {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("appName")
+    val conf = new SparkConf().setAppName("Streaming")
     val sc = new SparkContext(conf)
-
-
 
     val spark = SparkSession.builder().appName("Streaming").config(sc.getConf).getOrCreate()
     val ssc = new StreamingContext(sc, Seconds(2))
 
     val ds = ssc.socketTextStream("10.90.138.32", 8989)
 
-    val schema = new StructType().add(StructField("Sentiment", IntegerType, true)).add(StructField("Text", StringType, false))
+    val logisticRegressionModel = LogisticRegressionModel.load("logRegModel")
+    val schema = new StructType()
+      .add(StructField("Sentiment", IntegerType, true))
+      .add(StructField("Text", StringType, false))
+
     ds.foreachRDD(
       tweet => {
         val stream = spark.createDataFrame(tweet.map(attributes => Row(1, attributes)), schema)
-        stream.show()
+
+        if (!stream.isEmpty) {
+          val preprocessing = new Preprocessing(spark, 20, 20)
+          val df_test = preprocessing.prep_test(stream)
+
+          val predictions = logisticRegressionModel.transform(df_test)
+          predictions.write.mode(SaveMode.Append).csv("stream/pred.csv")
+        }
+
       }
     )
     ssc.start()
-    ssc.awaitTerminationOrTimeout(60 * 1000)
+    ssc.awaitTerminationOrTimeout(120 * 1000)
     ssc.stop()
   }
 }
