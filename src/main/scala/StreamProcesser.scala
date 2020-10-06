8bb5c538e63c5d91d70bc45f6f8b632a50deb363
Artem Bakhanov <artembakhanov@gmail.com>
2020-10-06 15:28:28 +0300

    Clean code

--- src/main/scala/StreamProcesser.scala
+++ src/main/scala/StreamProcesser.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{Row, SQLContext, SparkSession}
+import org.apache.spark.streaming.{Seconds, StreamingContext, Time}
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -9,11 +10,8 @@ object StreamProcesser {
     val sc = new SparkContext(conf)
 
 
-    import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
-    import org.apache.spark.sql.{Row, SQLContext, SparkSession}
-    import org.apache.spark.streaming.{Seconds, StreamingContext, Time}
-    import org.apache.spark.{SparkConf, SparkContext}
-    val spark = SparkSession.builder().appName("Training").config(sc.getConf).getOrCreate()
+
+    val spark = SparkSession.builder().appName("Streaming").config(sc.getConf).getOrCreate()
     val ssc = new StreamingContext(sc, Seconds(2))
 
     val ds = ssc.socketTextStream("10.90.138.32", 8989)
