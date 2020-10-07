0cfaf956ff8a174f5488ef5c0991fa0eaf5fe997
Kamil Kamaliev <gregoryfroydentaler23@gmail.com>
2020-10-07 18:05:37 +0300

    +svc to train and fixed f1

--- src/main/scala/TrainClassifier.scala
+++ src/main/scala/TrainClassifier.scala
@@ -1,10 +1,11 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.ml.classification.{LogisticRegression, RandomForestClassifier}
+import org.apache.spark.ml.classification.{LinearSVC, LogisticRegression, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{DataFrame, SparkSession, SQLContext}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.rdd.RDD
 
 object TrainClassifier {
 
@@ -45,13 +46,32 @@ object TrainClassifier {
     randomForestClassificationModel
   }
 
+  def train_SVC(df_train: DataFrame) = {
+    // init
+    // Note: the best parameters were achieved in the grid search (check out Classifiers.scala)
+    val svc = new LinearSVC()
+      .setAggregationDepth(2)
+      .setMaxIter(150)
+      .setThreshold(0.6)
+      .setRegParam(0.0)
+
+    //train
+    val svcModel = svc.fit(df_train)
+
+    // save
+    svcModel.save("svcModel")
+
+    svcModel
+  }
+
   // function for calculating f1 score
-  def calc_f1Score(sc: SparkContext, predictions: DataFrame): Tuple2[Double, Double] = {
+  def calc_f1Score(sc: SparkContext, predictions: DataFrame) = {
     val sqlContext= new SQLContext(sc)
     import sqlContext.implicits._
 
     // rdd storing tuples in the form of (probability, label)
-    val predictionsAndLabels = predictions.map(x => (x.getAs[Vector]("probability")(1), x.getAs[Double]("label"))).rdd
+    val predictionsAndLabels = predictions.map(x => (x.getAs[Double]("prediction"), x.getAs[Double]("label"))).rdd
+
 
     val metrics = new BinaryClassificationMetrics(predictionsAndLabels)
 
@@ -60,8 +80,9 @@ object TrainClassifier {
       .collect()
 
     // taking max value of f1 score
-    f1Score.maxBy(_._2) //Tuple2(threshold, f1 score)
+    f1Score.maxBy(_._1)._2 // f1 score
   }
+
 
 
   def main(args: Array[String]): Unit = {
@@ -104,17 +125,10 @@ object TrainClassifier {
     // printing the results
     println("Logistic Regression")
     predictions1.show(20, true)
-
     // evaluating the model
-    val evaluator1 = new BinaryClassificationEvaluator()
-      .setLabelCol("label")
-      .setRawPredictionCol("rawPrediction")
-      .setMetricName("areaUnderROC")
+    println(s"F1 score: ${calc_f1Score(sc, predictions1)}")
 
-    val accuracy1 = evaluator1.evaluate(predictions1)
-    print("Accuracy: ")
-    println(accuracy1)
-    println(s"The best f1 score: ${calc_f1Score(sc, predictions1)}")
+
 
     // training Random Forest Model
     val randomForestModel = train_RandForest(df_train)
@@ -123,18 +137,20 @@ object TrainClassifier {
     // printing the results
     println("Random Forest")
     predictions2.show(20, true)
-
-
     // evaluating the model
-    val evaluator2 = new BinaryClassificationEvaluator()
-      .setLabelCol("label")
-      .setRawPredictionCol("rawPrediction")
-      .setMetricName("areaUnderROC")
+    println(s"F1 score: ${calc_f1Score(sc, predictions2)}")
 
-    val accuracy2 = evaluator2.evaluate(predictions2)
-    print("Accuracy: ")
-    println(accuracy2)
-    println(s"The best f1 score: ${calc_f1Score(sc, predictions2)}")
+
+    // training Random Forest Model
+    val svcModel = train_SVC(df_train)
+    // testing the model
+    val predictions3 = svcModel.transform(df_test)
+    // printing the results
+    println("SVC")
+    predictions3.show(20, true)
+    // evaluating the model
+    println(s"F1 score: ${calc_f1Score(sc, predictions3)}")
+
 
   }
 }
